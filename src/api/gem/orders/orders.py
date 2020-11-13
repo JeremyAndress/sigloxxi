37b4955c91d6e960ea200f5644ad277a6cecbc08
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-11-13 20:13:49 -0300

    ADD: websocket ngnix configs

--- src/api/gem/orders/orders.py
+++ src/api/gem/orders/orders.py
@@ -1,13 +1,19 @@
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import (
+    APIRouter, Depends, HTTPException,
+    WebSocketDisconnect,WebSocket
+)
 from typing import List
 from db.session import get_db
 from schemas.orders import (
     OrdersBase, OrdersUpdate,
     OrdersDetailBase, OrdersDetailUpdate,
-    OrdersCompletedBase, OrdersCompletedUpdate,OrdersDetailCompletedUpdate,
-    OrdersDetailCompletedBase, OrdersDetailCompletedUpdate,
-    OrderList,ListOrdersDetail,ListOrdersCompleted,ListOrdersDetailCompleted
+    OrdersCompletedBase, 
+    OrdersCompletedUpdate,OrdersDetailCompletedUpdate,
+    OrdersDetailCompletedBase, 
+    OrdersDetailCompletedUpdate,
+    OrderList,ListOrdersDetail,
+    ListOrdersCompleted,ListOrdersDetailCompleted
 )
 from schemas.response import Response_SM
 from schemas.user import UserCreate,UserList
@@ -28,6 +34,37 @@ from .controller import (
     delete_orders_detail_completed_cn as dlt_ords_detail_completed
 )
 router = APIRouter()
+#var ws = new WebSocket("ws://localhost:8097/api/v1/ws");
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+@router.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
 @router.get("/orders/get_all_orders/",response_model=OrderList,tags=["admin"])
 def get_all_orders(
